d6d9e2f01a5095e789fab8bc4b2ecad2e2df7761
Gabriel <gbialystokjoly@gmail.com>
2023-04-08 10:11:15 -0400

    Added the approve command.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from asyncio import sleep
 from discord import User
 import os
 from dotenv import load_dotenv
+import asyncio
+from discord.ext import commands
 
 load_dotenv()
 token = str(os.getenv('TOKEN'))
@@ -27,4 +29,19 @@ async def suggest(ctx, *, suggestion: str):
     embed = discord.Embed(title='New Suggestion', description=f'{suggestion}', color=0x00ff00)
     embed.add_field(name="Author", value=ctx.message.author.mention, inline=False)
     await channel.send(embed=embed)
+
+@client.command()
+@commands.has_permissions(administrator=True)
+async def approve(ctx, user: User, suggestionMessageID: int,*, reason: str):
+    await ctx.message.delete()
+    channel = client.get_channel(969009498058530863)
+    message = await channel.fetch_message(suggestionMessageID)
+    embed = discord.Embed(title='Suggestion Approved', description=f'{reason}', color=0x00ff00)
+    embed.add_field(name="Administrator", value=ctx.message.author.mention, inline=False)
+    await user.send(embed=embed)
+    await message.reply(embed=embed)
+    
+
+
+
 client.run(token)
