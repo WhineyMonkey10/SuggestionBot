0240c893202bb4c898931ddb9ea97af6663d1e36
Gabriel <gbialystokjoly@gmail.com>
2023-04-08 10:23:18 -0400

    Added a ;customEmbed command.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 from dotenv import load_dotenv
 import asyncio
 from discord.ext import commands
+import re
 
 load_dotenv()
 token = str(os.getenv('TOKEN'))
@@ -51,6 +52,29 @@ async def deny(ctx, user: User, suggestionMessageID: int,*, reason: str):
     embed.add_field(name="Administrator", value=ctx.message.author.mention, inline=False)
     await user.send(embed=embed)
     await message.reply(embed=embed)
+
+@client.command()
+@commands.has_permissions(administrator=True)
+async def customEmbed(ctx):
+    await ctx.send('What would you like the title to be?')
+    title = await client.wait_for('message', check=lambda message: message.author == ctx.author)
+    title = title.content
+    await ctx.send('What would you like the description to be?')
+    description = await client.wait_for('message', check=lambda message: message.author == ctx.author)
+    description = description.content
+    await ctx.send('What would you like the footer to be?')
+    footer = await client.wait_for('message', check=lambda message: message.author == ctx.author)
+    footer = footer.content
+    await ctx.send('What channel would you like to send the embed to?')
+    channel = await client.wait_for('message', check=lambda message: message.author == ctx.author)
+    channel_mention = channel.content
+    channel_id = int(re.search(r'\d+', channel_mention).group())
+    channel = client.get_channel(channel_id)
+
+    await ctx.message.delete()
+    embed = discord.Embed(title=f'{title}', description=f'{description}', color=0x00ff00)
+    embed.set_footer(text=f'{footer}')
+    await channel.send(embed=embed)
 
     
 
